f56fc10bb3a62d01f81e44a29fefe246eedf3303
adrianomisina <adriano.misina@gmail.com>
2024-11-05 14:30:10 -0300

    feat: validacao e execoes no try exept

--- app.py
+++ app.py
@@ -20,16 +20,28 @@ def finalizar_app():
     os.system('clear')
     print('Finalizando o app :( \n')
 
+def opcao_invalida():
+    print('Opção inválida, tente novamente!\n')
+    input('Pressione qualquer tecla para continuar...')
+    os.system('clear')
+    exibir_opcoes()
+
+
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurantes')
-    else:
-        finalizar_app()
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        if opcao_escolhida == 1:
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+            print('Listar restaurantes')
+        elif opcao_escolhida == 3:
+            print('Ativar restaurantes')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
     exibir_nome_do_programa()
