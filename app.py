6e7f9b9c24b1ce14f03bc7e80cf6d34b01c30e2b
adrianomisina <adriano.misina@gmail.com>
2024-11-05 15:04:32 -0300

    feat: criação da lista de restaurantes

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = []
+
 def exibir_nome_do_programa(): 
     print("""
     ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -26,12 +28,20 @@ def opcao_invalida():
     os.system('clear')
     exibir_opcoes()
 
+def cadatrar_novo_restaurante():
+    os.system('clear')
+    print('Cadastro de novos restaurantes\n')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: \n')
+    restaurantes.append(f'{nome_do_restaurante} \n')
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso !\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
 
 def escolher_opcao():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
         if opcao_escolhida == 1:
-            print('Cadastrar restaurante')
+            cadatrar_novo_restaurante()
         elif opcao_escolhida == 2:
             print('Listar restaurantes')
         elif opcao_escolhida == 3:
